17f14d062b82e9cf0bfa32035beaedabb90bf3ef
Abby Anderson <abby.e.anderson@gmail.com>
2021-09-01 16:41:28 -0600

    commit tree baca1c69c9f4961d38d393f1428d27530524da6c

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+  })
 }
+
+
+//going to use .split() to split all the og strings into words, splitting at every space
+//then, split each word up so that we can uppercase only the first letter and then add on the remaining letters in each word
+//then, we'll have to add the titles back up, since all the words are individually in an array all together. so we will use join method
+
+
